34231da70c767e0ac58f6ae72ef876eab412ab7f
bigbulse <blaise.notter@outlook.com>
2022-01-21 15:28:46 -0500

    Lecture part 2

---
+++ Week01/class/syslogSU.py
@@ -0,0 +1,28 @@
+import syslogCheck
+import importlib
+importlib.reload(syslogCheck)
+
+# SSH authentication failuires
+def su_open(filename, searchTerms):
+    # Call syslogCheck and return the results
+    is_found = syslogCheck._syslog(filename, searchTerms)
+
+    # found list
+    found = []
+
+    # Loop through the results
+    for eachFound in is_found:
+
+        # split the results
+        sp_results = eachFound.split(" ")
+
+        # Append the split value to the found list
+        found.append(sp_results[5])
+
+    # Remove duplicates by using set
+    # and convert the list to a dict
+    retueredValues = set(found)
+
+    # print results
+    for eachValues in retueredValues:
+        print(eachValues)
