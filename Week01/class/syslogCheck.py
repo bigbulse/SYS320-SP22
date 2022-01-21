938ab48b9197bc4f1d75eba9135f601ee0eb6194
bigbulse <blaise.notter@outlook.com>
2022-01-21 15:47:54 -0500

    Linux 2k

--- Week01/class/syslogCheck.py
+++ Week01/class/syslogCheck.py
@@ -2,35 +2,34 @@
 import re
 import sys
 
-def _syslog(filename,listofKeywords):
-
+def _syslog(filename, listofKeywords):
     # Open a file
     with open(filename) as f:
-        # read in the file and save it to a variable
+        # read file and save it into a variable
         contents = f.readlines()
 
-    # Lists to store the results
+    # list to store our results
     results = []
 
-    # Loop through the list returned. Each element is a line from the smallSyslog file
+    # Loop through the list of lines returned, each element is a line from the small smallSyslog file
     for line in contents:
-        # Loops through the keywords
+        # loops through all of our keywords
         for eachKeyword in listofKeywords:
-            # If the 'line' contains the keyword then it will print
-            #if eachKeyword in line:
-            # Searches returned results using a regular expression search
+            # if the 'line' contains the keyword, then print it out
+            # if eachKeyword in line:
+            # searches and returns results using a regex search
             x = re.findall(r''+eachKeyword+'', line)
 
             for found in x:
-                # Append the returned keywords to the results list
+                # append the returned keywords to the results list
                 results.append(found)
 
-        # Check to see if there are results
-        if len(results) == 0:
-            print("No Results")
-            sys.exit(1)
+    # check to see if there are results
+    if len(results) == 0:
+        print("No Results")
+        sys.exit(1)
 
-        # sort the list
-        results = sorted(results)
+    # sort the list
+    results = sorted(results)
 
-        return results
+    return results
