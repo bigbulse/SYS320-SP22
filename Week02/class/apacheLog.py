893450e1d746272fe8451edd33b3d8f5c120efe0
bigbulse <blaise.notter@outlook.com>
2022-01-26 14:05:07 -0500

    Classwork without the access log

---
+++ Week02/class/apacheLog.py
@@ -0,0 +1,29 @@
+import logCheck
+import importlib
+importlib.reload(logCheck)
+
+# SSH authentication failuires
+def apache_events(filename, service, terms):
+    # Call syslogCheck and return the results
+    is_found = logCheck._log(filename, service, terms)
+
+    # found list
+    found = []
+
+    # Loop through the results
+    for eachFound in is_found:
+
+        # split the results
+        sp_results = eachFound.split(" ")
+
+        # Append the split value to the found list
+        # GET /cig-bin/test-cgi HTTP/1.1" 404 435 "-" "-"
+        found.append(sp_results[0] + " " + sp_results[1] + " " + sp_results[3])
+
+    # Remove duplicates by using set
+    # and convert the list to a dict
+    getValues = set(found)
+
+    # print results
+    for eachValue in getValues:
+        print(eachValue)
