08a6c308a649e90f3ae5a301277e3c5e75042712
Blaise Notter <59936853+bigbulse@users.noreply.github.com>
2022-02-09 17:05:54 -0500

    Add files via upload
    Video one completed

--- Week04/class/fs.py
+++ Week04/class/fs.py
@@ -3,7 +3,7 @@
 import os, sys
 
 # Get info from the commandline
-print(sys.argv)
+# print(sys.argv)
 
 # Directory to traverse
 rootdir = sys.argv[1]
@@ -12,8 +12,54 @@ rootdir = sys.argv[1]
 
 # In our story, we will traverse a directory
 if not os.path.isdir(rootdir):
-    print("Invalid Directory => {}").format(rootdir)
+    print("Invalid Directory => {}".format(rootdir))
     exit()
 
+# list to save files
+fList = []
+
 # Crawl through the provided directory
-# for root, subfolders, filenames(os.walk)
+for root, subfolders, filenames in os.walk(rootdir):
+    for f in filenames:
+        #print(root + "/" + f)
+        fileList = root + "/" + f
+        #print(fileList)
+        fList.append(fileList)
+
+# print(fList)
+
+def statFile(toStat):
+    # i is going to be the variable for each of the metadata elements
+    i = os.stat(toStat, follow_symlinks=False)
+
+    # mode
+    mode = i[0]
+
+    # inode 
+    inode=i[1]
+
+    # uid
+    uid = i[4]
+
+    # gid
+    gid = i[5]
+
+    # file size 
+    fsize = i[6]
+
+    # access time
+    atime = i[7]
+
+    # modification time
+    mtime = i[8]
+
+    # ctime => windows is the birth of the file, when it was created
+    # unix it is when attributes of the file changes.
+    ctime = i[9]
+    crtime = i[9]
+
+    print("0|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}".format(toStat, inode, mode, uid, gid, fsize, atime, mtime, ctime, crtime))
+
+for eachFile in fList:
+    statFile(eachFile)
+    # print(os.stat(eachFile))
